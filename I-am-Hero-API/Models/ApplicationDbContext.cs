e4b8c3ed9b26d6a7007edb423218f79420866dbc
Temirlan Yelemanov <36527788+TiTaniumum@users.noreply.github.com>
2025-01-21 21:15:26 +0500

    Добавил нужные пакеты из нугета а так же сделал временные настройки

---
+++ I-am-Hero-API/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace I_am_Hero_API.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
