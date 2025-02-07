b74eb03f8e3e45fefbbc8f541d2d7853c9e4e5f8
BIGGODDOG <Gann.ia98@gmail.com>
2025-02-07 18:20:15 +0500

    Strings.resx, LocalizationService.cs added

---
+++ I-am-Hero-WPF/Services/LocalizationService.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace I_am_Hero_WPF.Services
+{
+    public class LocalizationService
+    {
+        private ResourceManager _resourceManager;
+        private CultureInfo _currentCulture;
+
+        public LocalizationService()
+        {
+            _resourceManager = new ResourceManager("I_am_Hero_WPF.Resources.Strings", typeof(LocalizationService).Assembly);
+            _currentCulture = CultureInfo.CurrentCulture;
+        }
+
+        public string GetString(string key)
+        {
+            return _resourceManager.GetString(key, _currentCulture);
+        }
+
+        public void SetCulture(string cultureCode)
+        {
+            _currentCulture = new CultureInfo(cultureCode);
+        }
+    }
+}
